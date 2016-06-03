75e9172c050f7bf7613357c2e95de92579a63dcf
Elisabeth Handel <elisabeth-brigitte.handel@stud.uni-bamberg.de>
2016-06-03 14:13:30 +0200

    Displays funktionieren teilweise. Requests laufen.

--- TL2/AngewandteInformatik/js/ownjs.js
+++ TL2/AngewandteInformatik/js/ownjs.js
@@ -23,11 +23,42 @@ var textStatus = "Hat nicht funktioniert."
 
 
 **/
+var displayModuleDetails = function(data){
+	document.getElementById("moduleID").innerHTML = data.details.id;
+	document.getElementById("title").innerHTML = data.details.name;
 
+	if(data.details.minECTS == data.details.maxECTS){
+		document.getElementById("ects").innerHTML = data.details.minECTS;
+	}else{
+		document.getElementById("ects").innerHTML = "[" + data.details.minECTS + " - " + data.details.maxECTS + " ECTS-Punkte]";
+	}
+
+	/**for(i = 0; i <= data.details.courses.length; i++){
+		
+	}**/
+	var mandatory = "<h4> Pflichtmodule </h4> \n <table> <tr> <th>Kürzel</th> <th>Bezeichnung</th> <th>Semester</th> <th>ECTS</th> </tr> ";
+	var nonmandatory = "<h4> Wahlmodule </h4> \n <table> <tr> <th>Kürzel</th> <th>Bezeichnung</th> <th>Semester</th> <th>ECTS</th> </tr>";
+
+	$.each(data.details.courses, function(index, course){
+		if(course.mandatory){
+			mandatory = mandatory + "\n <tr> <td>" + course.short_name + "</td> </tr>";
+			mandatory = mandatory + 
+		} else {
+			nonmandatory = nonmandatory + "\n <tr> <td>" + course.short_name + "</td> </tr>"; 
+
+		}
+	})
+	mandatory = mandatory + "</table>";
+	nonmandatory = nonmandatory + "</table>"
+	var courses = mandatory + nonmandatory;
+	console.log(courses);
+	document.getElementById("courseList").innerHTML = courses;
+	
+}
 
 
 $(function(){
- $("#A1").click(function() {
+ $(".clickableModule").click(function() {
  	console.log("clicked");
 	var parameter = $(this).data('parameter'); 
 	$.ajax({
@@ -36,7 +67,8 @@ $(function(){
 	data: {module_details : parameter},
 	dataType: "json",
 	success: function(data){
-		document.getElementById("display").innerHTML = data.details.description;
+		console.log(data);
+		displayModuleDetails(data);
 	},
 	error: function(jqXHR, textStatus){
 		alert("Request failed" + textStatus)
