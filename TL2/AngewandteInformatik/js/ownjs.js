68880d370c70423f8d6d785690135e4d480e7c30
Elisabeth Handel <elisabeth-brigitte.handel@stud.uni-bamberg.de>
2016-06-03 13:05:21 +0200

    Ajaxrequests

--- TL2/AngewandteInformatik/js/ownjs.js
+++ TL2/AngewandteInformatik/js/ownjs.js
@@ -1,1 +1,47 @@
 //Hier steht unser eigenes javascript
+$(window).load($.get("php/moduleGroups.php"), function(){
+	//return document.innerHTML("<h3>Hallo</h3>")
+	console.log("Hallo")
+}, "json");
+
+var msg = "Hat funktioniert."
+var textStatus = "Hat nicht funktioniert."
+
+/**$.ajax({
+	url: "php/moduleGroups.php",
+	type: "GET",
+	data: {"?module_details=A1"},
+	dataType: "json",
+	success: function(msg){
+		console.log("funktioniert");
+	},
+	error: function(jqXHR, textStatus){
+		alert("Request failed" + textStatus)
+	}
+});
+
+
+**/
+
+
+
+$(function(){
+ $("#A1").click(function() {
+ 	console.log("clicked");
+	var parameter = $(this).data('parameter'); 
+	$.ajax({
+	url: "php/moduleGroups.php",
+	type: "GET",
+	data: {module_details : parameter},
+	dataType: "json",
+	success: function(data){
+		document.getElementById("display").innerHTML = data.details.description;
+	},
+	error: function(jqXHR, textStatus){
+		alert("Request failed" + textStatus)
+	}
+});
+});
+});
+
+
