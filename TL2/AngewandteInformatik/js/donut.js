36803d6a3e9092f9985f3b7955e9e06a1b5ff9c9
Sunflower90 <madeleine@rosenhagen.de>
2016-06-15 10:44:57 +0200

    started to do some changes on donut

--- TL2/AngewandteInformatik/js/donut.js
+++ TL2/AngewandteInformatik/js/donut.js
@@ -3,7 +3,7 @@
  function donut() 
  {
  var data = [54, 90, 78, 60, 18, 36, 24];
-var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
+var data2 = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'];
         var r = 300;
         
         var legendRectSize = 20;
@@ -15,6 +15,7 @@ var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
         var canvas = d3.select('#canvas').append('svg')
         .attr('width', 1000)
         .attr('height', 1000);
+		/** Hier die Höhe und Breite nicht fest setzen **/
         
 
         /**Zentrum von unserem Donut **/
@@ -35,10 +36,18 @@ var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
         /** Zuerst schicken wir Data an Pie-layout, jedes Group-Element hat die Klasse von Arc **/
 
         var arcs = group.selectAll('.arc')
-        .data(pie(data))
+        //.data(pie(data))
+		.data(data2)
         .enter()
         .append('g')
-        .attr('class', 'arc');
+        .attr('class', 'arc')
+		.on("mouseover", function(d){
+			/** Hier die Info im Kreisinnern **/
+			console.log("On Mouseover: " + d.value);
+		})
+		.on("click", function(d){
+			alert("Hier kommt ein clickable Event " + d.value);
+		});
 
         /** Für die oberen Objekten müssen wir Pfade einbinden, dann mit Farbe befüllt mittels function(d) **/
         arcs.append('path')
@@ -54,7 +63,7 @@ var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
         .append('g')
         .attr('class', 'legend');
         
-        
+        /**
         if(i == 0) {
         legend.append('text')
          .attr('x', -20)
@@ -66,7 +75,7 @@ var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
          
          }
         if(i == 1) {
-        /**Warum geht es nicht?! */
+        //Warum geht es nicht?! 
         d3.select('legend').remove();
         
         
@@ -94,7 +103,10 @@ var data2 = ['A1', 'A2', 'A3', 60, 18, 36, 24];
         else if(i==6) {
         a7(); 
         }
-      });       
+		**/
+      });  
+	  
+ 	  
 }
 function a1() {
             alert("Hier kommt ein clickable Event A1");
