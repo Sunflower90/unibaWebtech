9b756ae58bd676b8ba545689ef21fff9a95f1bad
Elisabeth Handel <elisabeth-brigitte.handel@stud.uni-bamberg.de>
2016-07-07 16:32:38 +0200

    StadionView angefangen

--- EM-WebTech/app/controllers/StadionController.java
+++ EM-WebTech/app/controllers/StadionController.java
@@ -1,28 +1,37 @@
 package controllers;
 
+import com.google.inject.Inject;
+
+import models.Stadion;
+import play.data.FormFactory;
+import play.mvc.Controller;
 import play.mvc.Result;
 
-public class StadionController {
-	
-	
+public class StadionController extends Controller {
+
+	@Inject
+	private FormFactory formFactory;
+
 	// Macht nur das Formular auf
-	public Result create(){
-	return null;
+	public Result create() {
+		return ok(views.html.stadionForm.render("Create", formFactory.form(Stadion.class)));
 	}
-	public Result delete(Long id){
+
+	public Result delete(Long id) {
 		return null;
 	}
-	public Result updateStadion(Long sId){
+
+	public Result updateStadion(Long sId) {
 		return null;
-		
+
 	}
-	
-	public Result storeStadion(){
-		return null;
-	}
-	
-	public Result readStadion(){
+
+	public Result storeStadion() {
 		return null;
 	}
-	
+
+	public Result readStadion() {
+		return null;
+	}
+
 }
