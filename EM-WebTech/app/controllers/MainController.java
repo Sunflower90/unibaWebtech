ced5d0c89e34209ee124533d4aa181957bb39048
Sunflower90 <madeleine@rosenhagen.de>
2016-07-11 14:44:19 +0200

    Added timelinePage Template

--- EM-WebTech/app/controllers/MainController.java
+++ EM-WebTech/app/controllers/MainController.java
@@ -9,4 +9,8 @@ public class MainController extends Controller {
 	public Result showStartPage(){
 		return ok(views.html.ourIndex.render("Korruptionssimulator", Partie.read(), Stadion.read()));
 	}
+	
+	public Result showTimelinePage(){
+		return ok(views.html.timeline.render("Korruptionssimulator - Timeline", Partie.read()));
+	}
 }
