3de189ba2d5e36ac59636333dc8fcf2462b99cf6
Sunflower90 <madeleine@rosenhagen.de>
2016-07-11 16:03:51 +0200

    did some stuff in timeline

--- EM-WebTech/app/models/Partie.java
+++ EM-WebTech/app/models/Partie.java
@@ -1,7 +1,9 @@
 package models;
 
+import java.time.Period;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -45,6 +47,40 @@ public class Partie extends Model{
 	public static void delete(Long pId) {
 		find.ref(pId).delete();
 	}
+	
+	public String getPeriod(){
+		long difference = date.getTime() - System.currentTimeMillis();
+		return getDurationBreakdown(difference);
+	}
+	
+	
+	 private static String getDurationBreakdown(long millis)
+	    {
+	        if(millis < 0)
+	        {
+	            throw new IllegalArgumentException("Duration must be greater than zero!");
+	        }
+
+	        long days = TimeUnit.MILLISECONDS.toDays(millis);
+	        millis -= TimeUnit.DAYS.toMillis(days);
+	        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+	        millis -= TimeUnit.HOURS.toMillis(hours);
+	        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+	        millis -= TimeUnit.MINUTES.toMillis(minutes);
+	        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+	        StringBuilder sb = new StringBuilder(64);
+	        sb.append(days);
+	        sb.append(" Days ");
+	        sb.append(hours);
+	        sb.append(" Hours ");
+	        sb.append(minutes);
+	        sb.append(" Minutes ");
+	        sb.append(seconds);
+	        sb.append(" Seconds");
+
+	        return(sb.toString());
+	    }
 
 	
 }
