00277b5ff3e1d7e6e933e41190f14101756c73fb
Sunflower90 <madeleine@rosenhagen.de>
2016-07-12 09:33:16 +0200

    partien now sorted

--- EM-WebTech/app/controllers/MainController.java
+++ EM-WebTech/app/controllers/MainController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import models.Partie;
 import models.Stadion;
 import play.mvc.*;
@@ -11,6 +15,16 @@ public class MainController extends Controller {
 	}
 	
 	public Result showTimelinePage(){
-		return ok(views.html.timeline.render("Korruptionssimulator - Timeline", Partie.read()));
+		return ok(views.html.timeline.render("Korruptionssimulator - Timeline", sortPartien(Partie.read())));
+	}
+	
+	private List<Partie> sortPartien(List<Partie> unsorted){
+		 Collections.sort(unsorted, new Comparator<Partie>() {
+			  public int compare(Partie p1, Partie p2) {
+				  return p1.date.compareTo(p2.date);
+			  }
+			});
+		 return unsorted;
+		
 	}
 }
